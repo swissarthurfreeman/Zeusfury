8f7dc3634877cacf856b917058a069b964f68f3c
unknown <swissarthurfreeman@gmail.com>
2023-05-27 20:10:07 +0200

    Revamped to Windows

---
+++ Assets/Scripts/LightningStrike.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using DigitalRuby.LightningBolt;
+using UnityEngine;
+
+public class LightningStrike : MonoBehaviour
+{
+    public Camera ZeusCam;
+    public GameObject LightningPrefab;
+    public float lightningTime = 0.5f;
+    public Tobii.Research.Unity.GazeTrailBase gazeTrail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    // Raycast, mousedown, see https://learn.unity.com/tutorial/onmousedown#
+    void LateUpdate()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = Input.mousePosition;
+            Ray ray = ZeusCam.ScreenPointToRay(mousePos);
+
+            // out passes parameter as reference, see 
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/out-parameter-modifier
+
+            if(Physics.Raycast(ray, out RaycastHit hit)) {  // true if intersects a collider
+                GameObject start = LightningPrefab.transform.GetChild(0).gameObject;
+                start.transform.position = transform.position + new Vector3(0, 0, 10);
+
+                GameObject end = LightningPrefab.transform.GetChild(1).gameObject;
+                end.transform.position = hit.point;
+                
+                LightningBoltScript test = LightningPrefab.GetComponent<LightningBoltScript>();
+                test.Trigger();
+            }
+        }     
+
+        if(Input.GetKeyDown(KeyCode.Return)) {
+            //Debug.Log(gazeTrail.latestHitPoint);
+            //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //cube.transform.position = gazeTrail.latestHitPoint;
+
+            GameObject start = LightningPrefab.transform.GetChild(0).gameObject;
+            start.transform.position = transform.position + new Vector3(0, 0, 10);
+
+            GameObject end = LightningPrefab.transform.GetChild(1).gameObject;
+            end.transform.position = gazeTrail.latestHitPoint;
+            
+            LightningBoltScript test = LightningPrefab.GetComponent<LightningBoltScript>();
+            test.Trigger();
+        }  
+    }
+}
---
+++ Assets/Scripts/LycaonController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LycaonController : MonoBehaviour
+{
+	[Header("Player")]
+	[Tooltip("Move speed of the character in m/s")]
+	public float MoveSpeed = 4.0f;
+
+	[Tooltip("Rotation speed of the character camera")]
+	public float rotateSpeed = 100;
+
+	[Space(10)]
+	[Tooltip("The height the player can jump")]
+	public float JumpHeight = 1.2f;
+	[Tooltip("The character uses its own gravity value.")]
+	public float Gravity = -15.0f;
+    
+	private CharacterController _controller;
+
+	private float _jumpTimeoutDelta;
+	private float _fallTimeoutDelta;
+
+	private float _speed;
+	private float _rotationVelocity;
+	private float _verticalVelocity;
+	private float _terminalVelocity = 53.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+		_controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+	private void Update() {
+		JumpAndGravity();
+		Move();
+	}
+
+	private void JumpAndGravity() {
+		if (Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded)  // Jump
+			_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);   // the square root of H * -2 * G = how much velocity needed to reach desired height
+
+		if (Math.Abs(_verticalVelocity) < Math.Abs(_terminalVelocity))
+			_verticalVelocity += Gravity * Time.deltaTime;
+	}
+
+    private void Move() {
+		float vertInput = Input.GetAxis("LycaonMove");
+		float horiInput = Input.GetAxis("LycaonRotate");
+
+		transform.Rotate(Vector3.up, -horiInput * rotateSpeed * Time.deltaTime);
+		
+		_controller.Move(
+			transform.forward * vertInput * (MoveSpeed * Time.deltaTime) + 
+			new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime
+		);
+	}
+}
