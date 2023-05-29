c5df98c6f7d30a3b464b674b9ecd7351737bf46b
unknown <swissarthurfreeman@gmail.com>
2023-05-29 19:43:37 +0200

    Animated Lycaon and revamped jumping mechanics

--- Assets/Scripts/LycaonController.cs
+++ Assets/Scripts/LycaonController.cs
@@ -8,33 +8,21 @@ public class LycaonController : MonoBehaviour
 {
 	[Header("Player")]
 	[Tooltip("Move speed of the character in m/s")]
-	public float MoveSpeed = 4.0f;
-
+	public float moveSpeed = 4.0f;
 	[Tooltip("Rotation speed of the character camera")]
-	public float rotateSpeed = 100;
-
-	[Space(10)]
-	[Tooltip("The height the player can jump")]
-	public float JumpHeight = 1.2f;
-	[Tooltip("The character uses its own gravity value.")]
-	public float Gravity = -15.0f;
-    
+	public float rotateSpeed = 100;    
+	private float verticalVelocity;
+    private float groundedTimer;        // to allow jumping when going down ramps
+    public float jumpHeight = 8.0f;
+    public float gravityValue = 9.81f;
 	private CharacterController _controller;
-
-	private float _jumpTimeoutDelta;
-	private float _fallTimeoutDelta;
-
-	private float _speed;
-	private float _rotationVelocity;
-	private float _verticalVelocity;
-	private float _terminalVelocity = 53.0f;
+	public GameObject LycaonBody;
+	private Animator LycaonBodyAnimator;
     // Start is called before the first frame update
     void Start() {
 		_controller = GetComponent<CharacterController>();
 		GameObject mainPanel = GameObject.Find("MainPanel");
-		//if(mainPanel != null) {
-		//	_bitalino = mainPanel.GetComponent<Hybrid8Test>();
-		//}
+		LycaonBodyAnimator = LycaonBody.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -42,15 +30,32 @@ public class LycaonController : MonoBehaviour
 		JumpAndGravity();
 		Move();
 	}
-
 	private void JumpAndGravity() {
-		if (Input.GetKeyDown(KeyCode.Space) && _controller.isGrounded)  // Jump
-			_verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);   // the square root of H * -2 * G = how much velocity needed to reach desired height
+		bool groundedPlayer = _controller.isGrounded;
+        if (groundedPlayer)
+            groundedTimer = 0.2f;	// cooldown interval to allow reliable jumping even whem coming down ramps
 
-		if (Math.Abs(_verticalVelocity) < Math.Abs(_terminalVelocity))
-			_verticalVelocity += Gravity * Time.deltaTime;
+        if (groundedTimer > 0)
+            groundedTimer -= Time.deltaTime;
+ 
+        // slam into the ground
+        if (groundedPlayer && verticalVelocity < 0)
+            verticalVelocity = 0f;	// hit ground
+ 
+        verticalVelocity -= gravityValue * Time.deltaTime;	// apply gravity always, to let us track down ramps properly
+ 
+        // allow jump as long as the player is on the ground
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (groundedTimer > 0) {	// must have been grounded recently to allow jump
+                groundedTimer = 0;		// no more until we recontact ground
+				LycaonBodyAnimator.SetTrigger("Jump_trig");
+                verticalVelocity += Mathf.Sqrt(jumpHeight * 2 * gravityValue);
+            }
+        }
+        _controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);	// call .Move() once only
 	}
 
+	private bool idle = true;
     private void Move() {
 		float vertInput = Input.GetAxis("LycaonMove");
 		float horiInput = Input.GetAxis("LycaonRotate");
@@ -58,16 +63,32 @@ public class LycaonController : MonoBehaviour
 		transform.Rotate(Vector3.up, -horiInput * rotateSpeed * Time.deltaTime);
 		
 		_controller.Move(
-			transform.forward * vertInput * (MoveSpeed * Time.deltaTime) + 
-			new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime
+			transform.forward * vertInput * (moveSpeed * Time.deltaTime)
 		);
+
+		if(_controller.velocity.magnitude > 0) {
+			LycaonBodyAnimator.SetBool("Static_b", false);
+			LycaonBodyAnimator.SetFloat("Speed_f", 1.0f);
+		} else {
+			LycaonBodyAnimator.SetBool("Static_b", true);
+			LycaonBodyAnimator.SetFloat("Speed_f", 0f);
+		}
 	}
 
-	
-	public volatile float HeartRate;
-	private volatile int _n_beats = 0;
+	public float HeartRate;
+	private int _n_beats = 0;
+	private int _n_counts = 0;
 	public void Print(int value) {
-		Debug.Log("Mean value =" + value);
+		_n_counts += 1;
+
+		if(value > 600)
+			_n_beats += 1;
+
+		if(_n_counts == 50) {	// 5 secs measure
+			_n_counts = 0;
+			Debug.Log("BPM = " + _n_beats * 6);
+			_n_beats = 0;
+		}
 
 	}
 }
